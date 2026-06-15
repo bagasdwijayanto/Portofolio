c6a343d569825c35888d526b814b5da12c005777
bagasdwijayanto <bagasdwijayanto760@gmail.com>
2026-06-15 15:07:49 +0700

    🚀 Initial commit: Bagas Dwijayanto Portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // ganti 'portfolio' sesuai nama repo kamu di GitHub
+  base: '/Portfolio/', // ganti 'portfolio' sesuai nama repo kamu di GitHub
 })
